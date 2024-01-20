fd497fc926523af9df772e50b342850e9fa6a599
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2024-01-20 17:34:28 -0300

    feat: :art: rem to px

--- src/pages/EditApiPaths/index.tsx
+++ src/pages/EditApiPaths/index.tsx
@@ -7,63 +7,89 @@ import { PathGroupContainer } from './components/PathGroupContainer'
 import * as T from './types'
 import './styles.css'
 
-const PATH_GROUPS_MOCK: T.PathItemContrainerProps[] = [
+const PATH_GROUPS_MOCK: T.PathGroupContainerProps[] = [
   {
-    method: 'get',
-    pathRoute: '/users',
-    pathDescription: 'List all users',
-    authenticationsSection: [],
-    routeParamsSection: [],
-    queryParamsSection: [
-      { name: 'example', type: 'boolean', description: 'Example of query param' },
-      { name: 'example', type: 'boolean', description: 'Example of query param' },
-    ],
-    requestBodiesSection: [
-      { name: 'My Request Model #1', type: 'Application/JSON' },
-      { name: 'My Request Model #2', type: 'Application/JSON' },
-    ],
-    responseBodiesSection: [
-      { name: '400 My Response Model #1', type: 'Application/JSON' },
-      { name: '401 My Response Model #2', type: 'Application/JSON' },
-    ],
+    pathGroupName: 'Users',
+    pathItems: [
+      {
+        method: 'get',
+        pathRoute: '/users',
+        pathDescription: 'List all users',
+        authenticationsSection: [],
+        routeParamsSection: [],
+        queryParamsSection: [
+          { name: 'example', type: 'boolean', description: 'Example of query param' },
+          { name: 'example', type: 'boolean', description: 'Example of query param' },
+        ],
+        requestBodiesSection: [
+          { name: 'My Request Model #1', type: 'Application/JSON' },
+          { name: 'My Request Model #2', type: 'Application/JSON' },
+        ],
+        responseBodiesSection: [
+          { name: '400 My Response Model #1', type: 'Application/JSON' },
+          { name: '401 My Response Model #2', type: 'Application/JSON' },
+        ],
+      },
+      {
+        method: 'post',
+        pathRoute: '/users',
+        pathDescription: 'Create user',
+        authenticationsSection: [],
+        routeParamsSection: [],
+        queryParamsSection: [],
+        requestBodiesSection: [
+          { name: 'My Request Model #1', type: 'Application/JSON' },
+          { name: 'My Request Model #2', type: 'Application/JSON' },
+        ],
+        responseBodiesSection: [
+          { name: '400 My Response Model #1', type: 'Application/JSON' },
+          { name: '401 My Response Model #2', type: 'Application/JSON' },
+        ],
+      },
+      {
+        method: 'put',
+        pathRoute: '/users/{id}',
+        pathDescription: 'Update user',
+        authenticationsSection: [
+          { name: 'Customer Authentication', description: '(Bearer Token)' },
+        ],
+        routeParamsSection: [
+          { name: 'id', type: 'integer', description: 'User ID to be updated' }
+        ],
+        queryParamsSection: [
+          { name: 'example', type: 'boolean', description: 'Example of query param' },
+          { name: 'example', type: 'boolean', description: 'Example of query param' },
+        ],
+        requestBodiesSection: [
+          { name: 'My Request Model #1', type: 'Application/JSON' },
+          { name: 'My Request Model #2', type: 'Application/JSON' },
+        ],
+        responseBodiesSection: [
+          { name: '400 My Response Model #1', type: 'Application/JSON' },
+          { name: '401 My Response Model #2', type: 'Application/JSON' },
+        ],
+      }
+    ]
   },
   {
-    method: 'post',
-    pathRoute: '/users',
-    pathDescription: 'Create user',
-    authenticationsSection: [],
-    routeParamsSection: [],
-    queryParamsSection: [],
-    requestBodiesSection: [
-      { name: 'My Request Model #1', type: 'Application/JSON' },
-      { name: 'My Request Model #2', type: 'Application/JSON' },
-    ],
-    responseBodiesSection: [
-      { name: '400 My Response Model #1', type: 'Application/JSON' },
-      { name: '401 My Response Model #2', type: 'Application/JSON' },
-    ],
-  },
-  {
-    method: 'put',
-    pathRoute: '/users/{id}',
-    pathDescription: 'Update user',
-    authenticationsSection: [
-      { name: 'Customer Authentication', description: '(Bearer Token)' },
-    ],
-    routeParamsSection: [
-      { name: 'id', type: 'integer', description: 'User ID to be updated' }
-    ],
-    queryParamsSection: [
-      { name: 'example', type: 'boolean', description: 'Example of query param' },
-      { name: 'example', type: 'boolean', description: 'Example of query param' },
-    ],
-    requestBodiesSection: [
-      { name: 'My Request Model #1', type: 'Application/JSON' },
-      { name: 'My Request Model #2', type: 'Application/JSON' },
-    ],
-    responseBodiesSection: [
-      { name: '400 My Response Model #1', type: 'Application/JSON' },
-      { name: '401 My Response Model #2', type: 'Application/JSON' },
+    pathGroupName: 'Posts',
+    pathItems: [
+      {
+        method: 'get',
+        pathRoute: '/posts',
+        pathDescription: 'List all posts',
+        authenticationsSection: [],
+        routeParamsSection: [],
+        queryParamsSection: [
+          { name: 'example', type: 'boolean', description: 'Example of query param' },
+        ],
+        requestBodiesSection: [
+          { name: 'My Request Model #1', type: 'Application/JSON' },
+        ],
+        responseBodiesSection: [
+          { name: '400 My Response Model #1', type: 'Application/JSON' },
+        ],
+      },
     ],
   }
 ]
@@ -81,10 +107,13 @@ export const EditApiPaths: React.FC = () => {
           Icon={<Plus size={24} />}
         />
 
-        <PathGroupContainer
-          pathGroupName="Users"
-          pathItems={PATH_GROUPS_MOCK}
-        />
+        {PATH_GROUPS_MOCK.map((pathGroup, idx) => (
+          <PathGroupContainer
+            key={idx}
+            pathGroupName={pathGroup.pathGroupName}
+            pathItems={pathGroup.pathItems}
+          />
+        ))}
       </div>
     </div>
   )
